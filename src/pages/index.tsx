de6ea576837739feebd7fc2f1438b3bb842f69e6
Alan del Fresno <alandelfresno2@gmail.com>
2023-04-26 12:51:44 -0300

    Basic navbar implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React, { MouseEventHandler } from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import Navbar from '@/components/Navbar';
 
 const Home = (props: { toggleTheme: MouseEventHandler<HTMLAnchorElement> }) => {
+  
+
   return (
     <Box height="100%">
-      <Button onClick={props.toggleTheme as any}>Toggle Theme</Button>
+      <Box display='flex' justifyContent='flex-end'>
+        <Navbar toggleTheme={props.toggleTheme} />
+      </Box>
       <Typography color="primary"> testing </Typography>
     </Box>
   );
