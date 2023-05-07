44ec11f82e263cab3f0ef69b553558ea36b0cea7
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-07 18:30:57 -0300

    Creation of Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <Box>
+      <Typography> Footer test</Typography>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Bio from '@/components/Bio';
 import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
 
 
 const Home = (props: { toggleTheme: MouseEventHandler<HTMLAnchorElement> }) => {
@@ -20,6 +21,7 @@ const Home = (props: { toggleTheme: MouseEventHandler<HTMLAnchorElement> }) => {
       {/* <Where I worked /> */}
       <Projects />
       <Contact />
+      <Footer />
     </Box>
   );
 };
