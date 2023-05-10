d2c39507edb975b2774317385156a3690b6df372
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-10 10:25:08 -0300

    Contact Refactor

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <Box display="flex" justifyContent="center" marginTop="6rem">
-      <Box display="flex" flexDirection="column">
+      <Box flexDirection="column">
         <Typography>Contact me</Typography>
         <Typography>Alandelfresno2@gmail.com</Typography>
         <Typography>Linkedin:</Typography>
