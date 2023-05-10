132385f84fc5fe9cebf9fe099a574a184502ad9b
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-10 11:44:03 -0300

    Experience structure improvement
    Creation of the content and change in structure

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,10 +5,37 @@ const Experience = () => {
   return (
     <Box
       display="flex"
+      flexDirection="column"
       justifyContent="center"
       marginTop="6rem"
     >
-      <Typography>Experience</Typography>
+      <Box
+        display="flex"
+        justifyContent="space-around"
+        width="60%"
+        alignSelf="center"
+      >
+        <Box border="1px solid" borderRadius="0.5rem" marginTop="1rem">
+          <Box margin="0.5rem">
+            <Typography variant="h4">Full Stack Developer</Typography>
+            <Box display="flex">
+              <Typography variant="h5">Cobuild Lab</Typography>
+              <Typography marginLeft="1rem" alignSelf="end">
+                Oct 2022 - Feb 2023 | 5mos
+              </Typography>
+            </Box>
+            <Typography variant="body1" marginTop="0.5rem">
+              Skills used:
+            </Typography>
+            <Typography variant="body2" marginTop="0.25rem">
+              NextJS, TypeScript, Hasura API, GraphQL (Apollo)
+            </Typography>
+          </Box>
+        </Box>
+        <Typography variant="h3" justifySelf="center">
+          Experience
+        </Typography>
+      </Box>
     </Box>
   );
 };
