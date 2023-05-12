b9630a58cdfeffb292d2069eac01c28030e4cda6
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-12 09:23:49 -0300

    Icons link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Box, Link, Typography } from '@mui/material';
-import { LinkedIn, GitHub } from '@mui/icons-material';
-
+import { LinkedIn, GitHub, Email} from '@mui/icons-material';
 const Contact = () => {
   return (
     <Box display="flex" justifyContent="center" marginTop="6rem">
       <Box flexDirection="column">
-        <Typography variant="h3">Contact me:</Typography>
+        <Typography variant="h4">Contact me:</Typography>
         <Box display="flex" justifyContent="center" marginTop="2rem">
-          <Typography variant="h5">Alandelfresno2@gmail.com</Typography>
         </Box>
-        <Box display="flex" justifyContent="space-around" marginTop="1rem">
+        <Box display="flex" justifyContent="space-evenly" marginTop="1rem">
+        <a
+            target="_blank"
+            href="mailto:alandelfresno2@gmail.com"
+            style={{ color: 'inherit', transform: 'scale(1.7)' }}
+          >
+            <Email />
+          </a>
           <a
             target="_blank"
             href="https://www.linkedin.com/in/alandelfresno/"
