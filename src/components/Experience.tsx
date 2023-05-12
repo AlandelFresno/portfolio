a141038528f2c104b587e4cd147caea4cf377d27
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-12 09:33:54 -0300

    Style change

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,7 +7,8 @@ const Experience = () => {
       display="flex"
       flexDirection="column"
       justifyContent="center"
-      marginTop="6rem"
+      marginTop="10rem"
+      marginBottom="15rem"
     >
       <Box
         display="flex"
@@ -15,22 +16,20 @@ const Experience = () => {
         width="60%"
         alignSelf="center"
       >
-        <Box border="1px solid" borderRadius="0.5rem" marginTop="1rem">
-          <Box margin="0.5rem">
-            <Typography variant="h4">Full Stack Developer</Typography>
-            <Box display="flex">
-              <Typography variant="h5">Cobuild Lab</Typography>
-              <Typography marginLeft="1rem" alignSelf="end">
-                Oct 2022 - Feb 2023 | 5mos
-              </Typography>
-            </Box>
-            <Typography variant="body1" marginTop="0.5rem">
-              Skills used:
-            </Typography>
-            <Typography variant="body2" marginTop="0.25rem">
-              NextJS, TypeScript, Hasura API, GraphQL (Apollo)
+        <Box marginBottom="0.5rem" marginLeft="0.5rem" marginRight="0.5rem">
+          <Typography variant="h4">Full Stack Developer</Typography>
+          <Box display="flex">
+            <Typography variant="h5">Cobuild Lab</Typography>
+            <Typography marginLeft="1rem" alignSelf="end">
+              Oct 2022 - Feb 2023 | 5mos
             </Typography>
           </Box>
+          <Typography variant="body1" marginTop="0.5rem">
+            Skills used:
+          </Typography>
+          <Typography variant="body2" marginTop="0.25rem">
+            NextJS, TypeScript, Hasura API, GraphQL (Apollo)
+          </Typography>
         </Box>
         <Typography variant="h3" justifySelf="center">
           Experience
