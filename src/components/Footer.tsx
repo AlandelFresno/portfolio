ac3cce210cd689611a3304d26a3e544d5082fa98
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-13 21:20:23 -0300

    Footer basic structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,29 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const Footer = () => {
+  const isBrowser = () => typeof window !== 'undefined';
+
+  function scrollToTop() {
+    if (!isBrowser()) return;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
   return (
-    <Box>
-      <Typography> Footer test</Typography>
+    <Box
+      display="flex"
+      justifyContent="space-around"
+      marginTop="10rem"
+      paddingBottom="2rem"
+    >
+      <Box>
+        <Typography> Email:</Typography>
+        <Typography> alandelfresno2@gmail.com</Typography>
+      </Box>
+      <Box >
+        <KeyboardArrowUpIcon onClick={scrollToTop} cursor="pointer"  transform='scale(2)'/>
+      </Box>
     </Box>
   );
 };
