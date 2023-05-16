730032cfc6027a5866effddf8a1909909aab098c
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-16 10:44:32 -0300

    Skill structure change
    Plus responsive

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,96 +1,164 @@
 import React from 'react';
-import { Box, List, ListItem, Typography } from '@mui/material';
+import { Box, List, ListItem, Typography, styled } from '@mui/material';
+
+const ResponsiveSkills = styled('div')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  h5: {
+    maxWidth: '150px',
+    width: '150px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  [theme.breakpoints.up(1180)]: {
+    paddingTop: '4rem',
+  },
+  [theme.breakpoints.down(1180)]: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    h5: {
+      marginTop: '2rem',
+    },
+  },
+}));
 
 const Skills = () => {
-  const degrees = 360;
-
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      marginTop="10rem"
-    >
-      <Box alignSelf="center">
-        <Typography
-          variant="h5"
-          border="2px solid"
-          borderRadius="2rem"
-          padding="0.5rem"
-        >
-          TypeScript
-        </Typography>
+    <Box display="flex" justifyContent="center" marginTop="10rem">
+      <Box flexDirection="column" width="70%">
+        <Box display="flex" justifyContent="center">
+          <Typography variant="h3">Skills</Typography>
+        </Box>
+        <ResponsiveSkills>
+          <Typography
+            variant="h5"
+            border="2px solid"
+            borderRadius="2rem"
+            padding="0.5rem"
+          >
+            JavaScript
+          </Typography>
+          <Typography
+            variant="h5"
+            border="2px solid"
+            borderRadius="2rem"
+            padding="0.5rem"
+          >
+            TypeScript
+          </Typography>
+          <Typography
+            variant="h5"
+            border="2px solid"
+            borderRadius="2rem"
+            padding="0.5rem"
+          >
+            React
+          </Typography>
+          <Typography
+            variant="h5"
+            border="2px solid"
+            borderRadius="2rem"
+            padding="0.5rem"
+          >
+            NextJS
+          </Typography>
+          <Typography
+            variant="h5"
+            border="2px solid"
+            borderRadius="2rem"
+            padding="0.5rem"
+          >
+            NodeJS
+          </Typography>
+        </ResponsiveSkills>
       </Box>
-      <Box
-        display="flex"
-        width="60%"
-        justifyContent="space-around"
-        alignSelf="center"
-      >
-        <Typography
-          variant="h5"
-          border="2px solid"
-          borderRadius="2rem"
-          padding="0.5rem"
-        >
-          JavaScript
-        </Typography>
-        <Typography
-          variant="h5"
-          border="2px solid"
-          borderRadius="2rem"
-          padding="0.5rem"
-        >
-          React
-        </Typography>
-      </Box>
-      <Box alignSelf="center">
-        <Typography variant="h2">Skills</Typography>
-      </Box>
-      <Box
-        display="flex"
-        width="60%"
-        justifyContent="space-around"
-        alignSelf="center"
-      >
-        <Typography
-          variant="h5"
-          border="2px solid"
-          borderRadius="2rem"
-          padding="0.5rem"
-        >
-          NextJS
-        </Typography>
-
-        <Typography
-          variant="h5"
-          border="2px solid"
-          borderRadius="2rem"
-          padding="0.5rem"
-        >
-          ExpressJS
-        </Typography>
-      </Box>
-      <Box alignSelf="center">
-        <Typography
-          variant="h5"
-          border="2px solid"
-          borderRadius="2rem"
-          padding="0.5rem"
-        >
-          NodeJS
-        </Typography>
-      </Box>
-      {/*
-        JavaScript
-        TypeScript
-        React
-        NextJS
-        NodeJS
-        ExpressJS 
-        GraphQL (Apollo)
-      */}
     </Box>
+    // <Box
+    //   display="flex"
+    //   flexDirection="column"
+    //   justifyContent="center"
+    //   marginTop="10rem"
+    // >
+    //   <Box alignSelf="center">
+    //     <Typography
+    //       variant="h5"
+    //       border="2px solid"
+    //       borderRadius="2rem"
+    //       padding="0.5rem"
+    //     >
+    //       TypeScript
+    //     </Typography>
+    //   </Box>
+    //   <Box
+    //     display="flex"
+    //     width="60%"
+    //     justifyContent="space-around"
+    //     alignSelf="center"
+    //   >
+    //     <Typography
+    //       variant="h5"
+    //       border="2px solid"
+    //       borderRadius="2rem"
+    //       padding="0.5rem"
+    //     >
+    //       JavaScript
+    //     </Typography>
+    //     <Typography
+    //       variant="h5"
+    //       border="2px solid"
+    //       borderRadius="2rem"
+    //       padding="0.5rem"
+    //     >
+    //       React
+    //     </Typography>
+    //   </Box>
+    //   <Box alignSelf="center">
+    //     <Typography variant="h2">Skills</Typography>
+    //   </Box>
+    //   <Box
+    //     display="flex"
+    //     width="60%"
+    //     justifyContent="space-around"
+    //     alignSelf="center"
+    //   >
+    //     <Typography
+    //       variant="h5"
+    //       border="2px solid"
+    //       borderRadius="2rem"
+    //       padding="0.5rem"
+    //     >
+    //       NextJS
+    //     </Typography>
+
+    //     <Typography
+    //       variant="h5"
+    //       border="2px solid"
+    //       borderRadius="2rem"
+    //       padding="0.5rem"
+    //     >
+    //       ExpressJS
+    //     </Typography>
+    //   </Box>
+    //   <Box alignSelf="center">
+    //     <Typography
+    //       variant="h5"
+    //       border="2px solid"
+    //       borderRadius="2rem"
+    //       padding="0.5rem"
+    //     >
+    //       NodeJS
+    //     </Typography>
+    //   </Box>
+    //   {/*
+    //     JavaScript
+    //     TypeScript
+    //     React
+    //     NextJS
+    //     NodeJS
+    //     ExpressJS
+    //     GraphQL (Apollo)
+    //   */}
+    // </Box>
   );
 };
 
