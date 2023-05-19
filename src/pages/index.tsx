c7e65a7cb23ca50fd1ccecc3b22bbc8265682b10
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-19 17:44:11 -0300

    Refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { MouseEventHandler } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import Navbar from '@/components/Navbar';
 import Header from '@/components/Header';
 import Bio from '@/components/Bio';
@@ -8,8 +8,6 @@ import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import Experience from '@/components/Experience';
-
-
 
 const Home = (props: { toggleTheme: MouseEventHandler<HTMLAnchorElement> }) => {
   return (
